d94d2b132a670624c55eb9da7af0a2ceb672aaa0
A.Sailesh Padmanabhan <arjun_sailesh@yahoo.com>
2021-09-23 12:55:13 +0530

    save changes to main file

--- mysrc/SayHello.cs
+++ mysrc/SayHello.cs
@@ -1,9 +1,10 @@
 using System;
 public class SayHello
   { 
+// This is main method
    public static void main(String args[]) 
   { 
-Console.WriteLine("Hello World");
+Console.WriteLine("Hello World!Welcome to CSharp!Happy coding");
   
  }
    }
